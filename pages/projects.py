1262a7ce3ce0e97b5729657e0f1b4ad37ad062e1
Erru Torculas <egtorculas@msi.upd.edu.ph>
2026-01-17 21:22:13 +0800

    added projects

--- pages/projects.py
+++ pages/projects.py
@@ -24,10 +24,10 @@ with st.container():
             st.space(3)
             with st.container(horizontal_alignment="right"):
                 st.page_link(label="View", icon=":material/visibility:", page="pages/projects/healthmap.py")
-    
-        col1, col2 = st.columns(2, border=True)
-    
+        
     with st.container():
+        col1, col2 = st.columns(2, border=True)
+
         with col1:
             with st.container(horizontal=True):
                 st.badge("Applied Research", icon=":material/co_present:", color="green")
@@ -48,5 +48,27 @@ with st.container():
             with st.container(horizontal=True, horizontal_alignment="right", vertical_alignment="center"):
                 st.link_button("Paper", type="tertiary", url="https://doi.org/10.1007/978-3-031-42430-4_35", icon=":material/attach_file:")
                 st.page_link(label="View", icon=":material/visibility:", page="pages/projects/aciids.py")
-                
+
+        
+    with st.container():
+        col1, col2 = st.columns(2, border=True)
+        
+        with col1:
+            with st.container(horizontal=True):
+                st.badge("Applied Research", icon=":material/co_present:", color="green")
+                st.badge("Special Problem/Undergraduate Thesis", icon=":material/podium:", color="violet")
+            st.markdown("**HanApp: A Missing Persons Reporting and Alerting Application Suite in the Philippines**")
+            st.caption("Missing persons cases remain a major issue in the Philippines, occurring not only after catastrophes but also in everyday situations. Existing attempts of the Philippine National Police (PNP) make scant use of modern technologies and lack community-driven processes. To fill these deficiencies, the researchers created HanApp, a mobile application that simplifies missing person reporting, case management, and verification while allowing for community-based search operations. The system was built using the Flutter Framework for cross-platform deployment and includes Google Location Services, Google Maps APIs, and Firebase for serverless backend, database, and storage.")
+            
+            with st.container(horizontal_alignment="right"):
+                st.link_button("Paper", type="tertiary", url="https://github.com/HanApp-MissingPersons/HanApp_SP/blob/main/Written%20Outputs/SP_FinalPaper/HanApp_SP_FinalPaper_FinalRev_SIGNED.pdf", icon=":material/attach_file:")
+            
+        
+        with col2:
+            st.badge("Concept Research", icon=":material/co_present:", color="yellow")
+            st.markdown("**A Hybrid BFM–ANFIS Modeling Framework for Predicting Phytoplankton Assemblage Composition and Productivity in Philippine Coastal Waters**")
+            st.caption("This project proposes a hybrid ecosystem modeling framework that integrates the mechanistic strengths of the Biogeochemical Flux Model coupled with NEMO (BFM-NEMO) and the adaptive learning capabilities of an Adaptive Neuro-Fuzzy Inference System (ANFIS) to improve predictions of phytoplankton assemblage composition and productivity. The framework is designed to enhance ecosystem-based management (EBM) by providing interpretable, data-driven, and process-aware forecasts of lower-trophic dynamics under climate variability and anthropogenic pressure in Philippine coastal systems")
+            st.space(3)
+            with st.container(horizontal_alignment="right"):
+                st.page_link(label="View", icon=":material/visibility:", page="pages/projects/healthmap.py", disabled=True)
     
